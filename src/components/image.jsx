b33f586bb825bbcafa50bb5f5f98d903d5daed5c
Joe Tustin <joetustin@joetustin.home>
2021-03-02 15:15:34 -0700

    adding in new state and useeffect

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+// import contentful from 'contentful'
 
-import contentful from 'contentful'
+const contentful = require('contentful')
 
 
 const client = contentful.createClient({
@@ -9,22 +9,41 @@ const client = contentful.createClient({
     accessToken: `${process.env.REACT_APP_CONTENTFUL_DELIVERY_API_TOKEN}`
 })
 
-// console.log(${process.env.REACT_APP_CONTENTFUL_DELIVERY_API_TOKEN})
-
-// const imageBaseUrl = 'https://images.ctfassets.net'
-
-function getAsset() {
-    client.getAsset('3WaafbaTPzK6gu40Don0No').then((asset) => console.log(asset))
-}
-
 
 function Image() {
+    const [imageInfo, setImageInfo] = useState({})
 
-    const image = getAsset()
+    useEffect(() => {
+        async function getImageInfo() {
+            const results = await client.getAsset('3WaafbaTPzK6gu40Don0No').then((entry) => entry).catch(error => console.error(error))
+            setImageInfo(results)
+        }
+        getImageInfo()
+        console.log("here")
 
-    console.log(image)
+        console.log("there")
+        // console.log(imageInfo, "image")
+
+        // console.log(result)
+    }, [])
+// console.log(getAsset)
+// console.log("hello")
+// console.log(imageInfo, "image")
+
+// console.log(result, "result")
+
+
+//    let imgURL = getAsset()
+    // console.log(getAsset())
+    // getAsset()
+
+
     return (
+        <div>
         <h2>Image</h2>
+        {/* <img src={imageInfo.fields.file.url} alt="pic"/> */}
+        </div>
+
     )
 }
 
