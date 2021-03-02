eafaa856e5b3bca15387a224aea985aa7b53b252
Joe Tustin <joetustin@joetustin.home>
2021-03-02 11:58:42 -0700

    connecting component and adding packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Image from './components/image'
+
 function App() {
   return (
     <div className="App">
       <h2>Hello</h2>
+      <Image />
     </div>
   );
 }
---
+++ src/components/image.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Image() {
+    return (
+        <h2>Image</h2>
+    )
+}
+
+export default Image
