9f3325ce4c256074a9a000277893b66282452626
Joe Tustin <joetustin@joetustin.home>
2021-03-02 12:13:09 -0700

    adding in env and connecting to contentful

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,6 +1,28 @@
 import React from 'react'
+import axios from 'axios'
+
+import contentful from 'contentful'
+
+
+const client = contentful.createClient({
+    space: '89124x1xlxyy',
+    accessToken: `${process.env.REACT_APP_CONTENTFUL_DELIVERY_API_TOKEN}`
+})
+
+// console.log(${process.env.REACT_APP_CONTENTFUL_DELIVERY_API_TOKEN})
+
+// const imageBaseUrl = 'https://images.ctfassets.net'
+
+function getAsset() {
+    client.getAsset('3WaafbaTPzK6gu40Don0No').then((asset) => console.log(asset))
+}
+
 
 function Image() {
+
+    const image = getAsset()
+
+    console.log(image)
     return (
         <h2>Image</h2>
     )
